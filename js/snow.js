74f5854d999cf9be78865cfb63df35910fd52c4f
Spec Ussa <serega.ussa@gmail.com>
2023-04-18 04:47:34 +0300

    fix

---
+++ js/snow.js
@@ -0,0 +1,59 @@
+// снег
+particlesJS (
+   "particles-dots", {
+   "particles": {
+     "number": {
+       "value": 30
+     },
+     "color": {
+       "value": "#eee"
+     },
+     "shape": {
+       "type": "circle"
+     },
+     "opacity": {
+       "value": 0.5,
+       "random": true,
+       "anim": {
+         "enable": false
+       }
+     },
+     "size": {
+       "value": 15,
+       "random": true,
+       "anim": {
+         "enable": false,
+       }
+     },
+     "line_linked": {
+       "enable": false
+     },
+     "move": {
+       "enable": true,
+       "speed": 1,
+       "direction": "none",
+       "random": true,
+       "straight": false,
+       "out_mode": "out"
+     }
+   },
+   "interactivity": {
+     "detect_on": "canvas",
+     "events": {
+       "onhover": {
+         "enable": false
+       },
+       "onclick": {
+         "enable": true,
+         "mode": "push"
+       },
+       "resize": true
+     },
+     "modes": {
+       "push": {
+         "particles_nb": 10
+       }
+     }
+   },
+   "retina_detect": true
+ }); 
