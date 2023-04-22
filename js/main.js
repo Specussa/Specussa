e4d37985e1642c1f28e164aa5ca18cf50fa847c4
Spec Ussa <serega.ussa@gmail.com>
2023-04-22 03:38:29 +0300

    clock

--- js/main.js
+++ js/main.js
@@ -41,3 +41,76 @@ btnDarkMode.onclick = function () {
     localStorage.setItem("darkMode", "light");
   }
 };
+
+// цифровые часы
+function time(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+function aclock() {
+var d = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
+d = new Date(d);
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  h = time(h);
+  m = time(m);
+  s = time(s);
+  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.getElementById("clock").innerHTML = days[d.getDay()]
+}
+aclock();
+window.setInterval(aclock, 1000);
+
+// аналоговые часы
+function dclock() {
+  var date = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
+  date = new Date(date),
+  hours = date.getHours(),
+  minutes = date.getMinutes(),
+  seconds = date.getSeconds();
+  
+  if (hours > 12) {
+  hours -= 12;
+  }
+  
+  var secondsStartDegree = 360 / 60 * seconds,
+  minutesStartDegree = 360 / 60 * minutes + 6 / 60 * seconds,
+  hoursStartDegree = 360 / 12 * hours + 30 / 60 * minutes + 0.5 / 60 * seconds;
+  
+  var style = document.createElement('style');
+  
+  style.type = 'text/css';
+  style.innerHTML = '\
+  @keyframes rotate--hour {\
+  from {transform: rotate(' + hoursStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (hoursStartDegree + 360) + 'deg) translate(-102px)}\
+  }\
+  @keyframes rotate--minute {\
+  from {transform: rotate(' + minutesStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (minutesStartDegree + 360) + 'deg) translate(-102px)}\
+  }\
+  @keyframes rotate--second {\
+  from {transform: rotate(' + secondsStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (secondsStartDegree + 360) + 'deg) translate(-102px)}\
+  }\
+  @-webkit-keyframes rotate--hour {\
+  from {transform: rotate(' + hoursStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (hoursStartDegree + 360) + 'deg) translate(-102px)}\
+  }\
+  @-webkit-keyframes rotate--minute {\
+  from {transform: rotate(' + minutesStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (minutesStartDegree + 360) + 'deg) translate(-102px)}\
+  }\
+  @-webkit-keyframes rotate--second {\
+  from {transform: rotate(' + secondsStartDegree + 'deg) translate(-102px)}\
+  to {transform: rotate(' + (secondsStartDegree + 360) + 'deg) translate(-102px)}\
+  }';
+  
+  document.getElementsByTagName('head')[0].appendChild(style);
+  }
+  dclock();
+  window.setInterval(dclock, 600000);
